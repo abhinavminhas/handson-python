84184839b0b1c0f9bd9a479539edbb2a37517ec7
abhinavminhas <abhinavminhas@users.noreply.github.com>
2022-09-25 13:42:25 +1000

    Python notes update

--- 1_python_basics/33_string_formatting.py
+++ 1_python_basics/33_string_formatting.py
@@ -14,9 +14,10 @@ print(txt.format(price))
     # You can add parameters inside the curly brackets to specify how to convert the value.
 txt = "The price is {:.2F} dollars"
 print(txt.format(price))
+
 print()
 # Multiple Values
-# If you want to use more values, just add more values to the format() method.
+    # If you want to use more values, just add more values to the format() method.
 quantity = 3
 itemno = 567
 price = 49
@@ -25,7 +26,7 @@ print(myorder.format(quantity, itemno, price))
 
 print()
 # Index Numbers
-# You can use index numbers (a number inside the curly brackets {0}) to be sure the values are placed in the correct placeholders.
+    # You can use index numbers (a number inside the curly brackets {0}) to be sure the values are placed in the correct placeholders.
 quantity = 3
 itemno = 567
 price = 49
@@ -33,7 +34,7 @@ myorder = "I want {0} pieces of item number {1} for {2:.2f} dollars."
 print(myorder.format(quantity, itemno, price))
 
 print()
-# Also, if you want to refer to the same value more than once, use the index number.
+    # Also, if you want to refer to the same value more than once, use the index number.
 age = 36
 name = "John"
 txt = "His name is {1}. {1} is {0} years old."
@@ -41,7 +42,7 @@ print(txt.format(age, name))
 
 print()
 # Named Indexes
-# You can also use named indexes by entering a name inside the curly brackets {carname}, 
-# but then you must use names when you pass the parameter values txt.format(carname = "Ford").
+    # You can also use named indexes by entering a name inside the curly brackets {carname}, 
+    # but then you must use names when you pass the parameter values txt.format(carname = "Ford").
 myorder = "I have a {carname}, it is a {model}."
 print(myorder.format(carname = "Ford", model = "Mustang"))
